ca45140babe56476691defc843aa50c891b3904f
heyyakash <akashsharma2002@gmail.com>
2023-04-26 03:56:26 +0530

    added new features

---
+++ types/projects.types.ts
@@ -0,0 +1,9 @@
+export type project =  {
+    id: number;
+    created_at: string;
+    name: string;
+    company: string;
+    team: string;
+    status: 'current' | 'completed';
+  }
+  
---
+++ types/tasks.types.ts
@@ -0,0 +1,15 @@
+export type taskstype =  {
+    id: number;
+    created_at: string;
+    company: string;
+    team: string;
+    task_category: string;
+    task_title: string;
+    task_desc: string;
+    assigned_to: string;
+    deadline_date: string;
+    status: string;
+    project: string;
+    user_image:string;
+  }
+  
